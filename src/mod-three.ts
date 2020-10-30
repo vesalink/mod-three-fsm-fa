d0cda43f0a1ee4af2ad17beaed3a4344ec6c229e
Sergey Fedyanin <vesalink@gmail.com>
2020-10-29 17:46:54 -0700

    upgrade typescript and comments

--- src/mod-three.ts
+++ src/mod-three.ts
@@ -8,6 +8,15 @@ export class binaryModThreeFSM {
     this.finalState = null;
   }
 
+  /**
+   * Calculate modulus 3 of binary representation of integer
+   * Uses Finite State Machine (FSM)
+   * Depends of the final state we can calculate the modulus 3: "S0" - 0, "S1" - 1, "S2" - 2
+   *
+   * @param {string} input String of 1's or 0's - binary
+   *
+   * @returns {number} Result of modulus 3 of representation of integer
+   */
   public modThree(input: string): number {
     if (input === "" || input === null) {
       return 0;
@@ -19,6 +28,15 @@ export class binaryModThreeFSM {
     return this.finalState === "S0" ? 0 : this.finalState === "S1" ? 1 : 2;
   }
 
+  /**
+   * Transition function
+   *
+   * @param {string} input Bit as a string "1" or "0"
+   *
+   * @param {string} currentState Current state of Finite automation
+   *
+   * @returns {string} Next state of Finite automation
+   */
   public transitions(input: string, currentState: string): string {
     if (
       (currentState === "S0" && input === "0") ||
